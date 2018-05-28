8d71befc15360e2266d5b724d24102a35045b876
Napat Wongcharoen <napat@que.co.th>
2018-05-28 17:29:33 +0700

    add discount code form and edit data

--- src/components/admin/AddPromotionCode.js
+++ src/components/admin/AddPromotionCode.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Input, Row, Col,
-Button } from 'reactstrap'
+Button, Form, FormGroup,
+Label } from 'reactstrap'
 import WrappedButton from '../common/WrappedButton'
 import AdminLayout1 from '../common/AdminLayout1'
 import AdminHeadings from '../common/AdminHeadings'
@@ -10,13 +11,36 @@ class AddPromotionCode extends Component {
     return (
       <AdminLayout1 headings='Add promotion code'>
         <AdminHeadings>
-          <h2>Add Promotion Code</h2>
+          <h2>Add Discount Code</h2>
         </AdminHeadings>
         <Row className='mt-3'>
-          <Col md='4' className='d-flex'>
-            <Input placeholder='Promotion Code' />
+          <Col md='5'>
+            <Form>
+              <FormGroup>
+                <Label for='discountCodeName'>Code name</Label>
+                <Input type='text' name='codeName' placeholder='name' />
+              </FormGroup>
+              <FormGroup>
+                <Label for='discountCodeName'>Discount Type</Label>
+                <Input type='select' name='discountType'>
+                  <option>Fixed</option>
+                  <option>Percent</option>
+                </Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for='amountDiscount'>Amount discount</Label>
+                <Input type='number' name='amountDiscount' />
+              </FormGroup>
+              <FormGroup>
+                <Label for='limitPeople'>Limit People</Label>
+                <Input type='number' name='limitPeople' />
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Col md='5' className='text-right'>
             <WrappedButton
-              className='ml-2'
               onClick={() => console.log('add action')}
               iconName='add'
               textcolor='#000'
